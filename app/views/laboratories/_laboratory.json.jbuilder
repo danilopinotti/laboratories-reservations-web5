b13374b02e45ede1f2d665db4029faf4ac45fd74
Danilo Pinotti <danilopinotti@hotmail.com>
2016-11-28 19:31:27 -0200

    Template should be configured to Materialize

---
+++ app/views/laboratories/_laboratory.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! laboratory, :id, :name, :localization, :created_at, :updated_at
+json.url laboratory_url(laboratory, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122221640) do
+ActiveRecord::Schema.define(version: 20161122224456) do
 
-  create_table "periods", force: :cascade do |t|
-    t.time     "begin_at"
-    t.time     "end_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "laboratories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "localization"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
