5a5897c152bf1f1b0a168a35914e9262e6fe6ff3
Danilo Pinotti <danilopinotti@hotmail.com>
2016-12-03 21:13:58 -0200

    Formatted hour to show

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,12 @@
 class Period < ActiveRecord::Base
   has_many :begin_reserve, foreign_key: :begin_at_id, class_name: :Reserve
   has_many :end_reserve, foreign_key: :end_at_id, class_name: :Reserve
+
+  def begin_at_formated
+    self.begin_at.strftime("%H:%m")
+  end
+
+  def end_at_formated
+    self.end_at.strftime("%H:%m")
+  end
 end
